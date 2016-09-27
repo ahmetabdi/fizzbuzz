9034632c4a8960cf1d066c310f953eadad41cd92
Ahmet <ahmetabdi@gmail.com>
2016-09-27 12:25:58 -0700

    Create fizzbuzz spec

---
+++ spec/lib/fizz_buzz_spec.rb
@@ -0,0 +1,23 @@
+require 'fizz_buzz'
+
+RSpec.describe FizzBuzz do
+  describe '.calculate' do
+    context 'when the value is a multiple of 3' do
+      it 'returns Fizz' do
+        expect(FizzBuzz.calculate(3)).to eq('Fizz')
+      end
+    end
+
+    context 'when the value is a multiple of 5' do
+      it 'returns Buzz' do
+        expect(FizzBuzz.calculate(5)).to eq('Buzz')
+      end
+    end
+
+    context 'when the value is a multiple of 3 and 5' do
+      it 'returns FizzBuzz' do
+        expect(FizzBuzz.calculate(15)).to eq('FizzBuzz')
+      end
+    end
+  end
+end
